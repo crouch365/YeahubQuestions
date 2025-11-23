b793dfff26743ebebb97e13e3bf920084e8b3ab3
Crouch <crouch365md@gmail.com>
2025-11-23 21:47:22 +0500

    last refactoring

--- src/widgets/questions/ui/QuestionList.tsx
+++ src/widgets/questions/ui/QuestionList.tsx
@@ -1,14 +1,16 @@
 import { useGetQuestionsQuery } from "@/entities/questions/api/questionApi";
 import styles from "./styles.module.css";
 import { Question } from "@/entities/questions";
-import { useAppSelector } from "@/app/appStore";
+import { useAppDispatch, useAppSelector } from "@/app/appStore";
 import { filtersSelectors } from "@/features/filters-questions/model/filterSelector";
 import Pagination from "@/shared/pagination/ui/Pagination";
+import { filterAction } from "@/features/filters-questions";
 
 const QuestionList = () => {
+  const dispatch = useAppDispatch();
   const params = useAppSelector(filtersSelectors.selectQuestionFilterParams);
   const limit = useAppSelector(filtersSelectors.selectFilterLimit);
-  const currentPages = useAppSelector(filtersSelectors.selectFilterPage);
+  const currentPage = useAppSelector(filtersSelectors.selectFilterPage);
   const { data: questions } = useGetQuestionsQuery(params);
 
   return (
@@ -18,10 +20,12 @@ const QuestionList = () => {
           <Question key={question.id} question={question} />
         ))}
       </ul>
+
       <Pagination
-        currentPages={currentPages}
         total={questions?.total ?? 0}
         limit={limit}
+        currentPage={currentPage}
+        onChangePage={(page) => dispatch(filterAction.setPage(page))}
       />
     </div>
   );
